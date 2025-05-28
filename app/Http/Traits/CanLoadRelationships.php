fe3d85ed10cf87f4b930b1be6eba012c3a89ad09
dakshj31 <dakshjoshi1355@gmail.com>
2025-05-28 23:48:52 +0530

    7

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -3,6 +3,7 @@
 namespace App\Http\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
@@ -10,9 +11,9 @@ use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 trait CanLoadRelationships
 {
     public function LoadRelationships(
-        Model|QueryBuilder|EloquentBuilder $for,
+        Model|QueryBuilder|EloquentBuilder|HasMany $for,
         ?array $relations = null
-    ): Model|QueryBuilder|EloquentBuilder {
+    ): Model|QueryBuilder|EloquentBuilder|HasMany {
         $relations = $relations ?? $this->relations ?? [];
             foreach ($relations as $relation) {
             $for->when(
