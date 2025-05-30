4b818a6de7ec70e55bb824244b0ca793d9f9ee51
dakshj31 <dakshjoshi1355@gmail.com>
2025-05-30 22:43:02 +0530

    9

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')
+    ->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
 Route::post('/login', [AuthController::class, 'login']);
 
